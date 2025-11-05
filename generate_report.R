e849f3bed5649b0a8b42c6f0ffccd1e0ae0ba967
kristatraboulay <kristatraboulay@gmail.com>
2025-11-05 11:51:28 -0800

    Move AQHI calculations before file processing

--- generate_report.R
+++ generate_report.R
@@ -1,7 +1,7 @@
 rmarkdown::render(
   input = "Report_WVLibrary.Rmd",
   output_format = "pdf_document",
-  output_file = "Report_LynnValleyLibrary_Sept2025.pdf"
+  output_file = "Report_CapilanoLibrary_Sept2025.pdf"
   # params = list(
   #   year = 2025,
 
