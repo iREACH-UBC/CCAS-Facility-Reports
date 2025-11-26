761106bdf3eefb6ec0bfaac1674113ce6d6d79c5
kristatraboulay <kristatraboulay@gmail.com>
2025-11-26 12:15:09 -0800

    Fix bugs in sensor uptime function

--- libraries/helper_functions.R
+++ libraries/helper_functions.R
@@ -288,7 +288,7 @@ data_includes_time_change <- function(
   FALSE
 }
 
-# Assume data_df$date are in UTC posix, no date overlaps
+# Assume data_df$date is in UTC posix
 get_sensor_uptime <- function(
   data_df, target_start_date_char, target_end_date_char
 ) {
@@ -302,27 +302,28 @@ get_sensor_uptime <- function(
     is.na(data_df$NO) & is.na(data_df$NO2) &
       is.na(data_df$CO) & is.na(data_df$O3)
   )
-
-  print(post_outage_na_indices)
+  if (length(post_outage_na_indices) == 0) {
+    return(1)
+  }
   # indices relative to post_outage_na_indices
   start_post_outage_indices <- c(1, which(
     (post_outage_na_indices[1:(length(post_outage_na_indices) - 1)] + 1) !=
       post_outage_na_indices[2:length(post_outage_na_indices)]
   ) + 1)
   # indices relative to post_outage_na_indices
-  end_post_outage_indices <- c(
-    (start_post_outage_indices[2:length(start_post_outage_indices)] - 1),
-    length(post_outage_na_indices)
-  )
-  print(start_post_outage_indices)
-  print(end_post_outage_indices)
-
+  if (length(start_post_outage_indices) == 1) {
+    end_post_outage_indices <- length(post_outage_na_indices)
+  } else {
+    end_post_outage_indices <- c(
+      (start_post_outage_indices[2:length(start_post_outage_indices)] - 1),
+      length(post_outage_na_indices)
+    )
+  }
+  # Get indices of dataframe rows before/after sensor outage
   before_outage_indices <- post_outage_na_indices[start_post_outage_indices] - 1
   after_outage_indices <- post_outage_na_indices[end_post_outage_indices] + 1
 
-  print(before_outage_indices)
-  print(after_outage_indices)
-
+  # Get dates before/after sensor outage
   dates_before_outage <- as.POSIXct(data_df$date[before_outage_indices])
   dates_after_outage <- as.POSIXct(data_df$date[after_outage_indices])
   if (before_outage_indices[[1]] == 0) {
@@ -330,30 +331,27 @@ get_sensor_uptime <- function(
   }
   if (after_outage_indices[[
     length(after_outage_indices)
-  ]] == (length(after_outage_indices) + 1)) {
+  ]] == (nrow(data_df) + 1)) {
     dates_after_outage[[length(dates_after_outage)]] <- last_expected_df_date
   }
-  print(dates_before_outage)
-  print(dates_after_outage)
+
+  # Calculate number of dates missed due to outages
   diff_minutes <- as.double(difftime(
     dates_after_outage, dates_before_outage, units = "mins"
   ))
-  print(diff_minutes)
-
   times_missed <- round((diff_minutes - 15) / 15)
   num_times_missed <- sum(times_missed)
-  # Are target dates needed?
-  # Get total dates
+
+  # Get total number of expected dates and sensor uptime
   total_num_times <- nrow(data_df) + num_times_missed -
     length(post_outage_na_indices)
   (total_num_times - num_times_missed) / total_num_times
 }
 
-uptime <- get_sensor_uptime(readr::read_csv(
-  "outdoor_data_processed/October2025/MOD-00631.csv",
-  show_col_types = FALSE
-), "2025-10-01", "2025-10-31")
-
+# uptime <- get_sensor_uptime(readr::read_csv(
+#   "test_data.csv",
+#   show_col_types = FALSE
+# ), "2025-10-01", "2025-10-31")
 
 # times <- c(
 #   "2025-11-02 00:00:00",
